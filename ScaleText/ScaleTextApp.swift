3cce738ca2182f7c5db5d81cbe76493875c1ce3c
Boris Bugor <bugor.bn@icloud.com>
2024-05-13 18:45:10 +0300

    Initial Commit

---
+++ ScaleText/ScaleTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaleTextApp.swift
+//  ScaleText
+//
+//  Created by Boris Bugor on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScaleTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
